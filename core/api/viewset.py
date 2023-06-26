f3df978df406010457390537a0da947d10e972da
fabio <fabio.freitas@les.inf.puc-rio.br>
2023-06-26 16:56:50 -0300

    refactor: Alterar numero de paginas na paginacao

--- core/api/viewset.py
+++ core/api/viewset.py
@@ -7,7 +7,7 @@ from .serializers import OccurrenceSerializer
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
