37c2dcb7676b13e3fc362c1dddc55fb0ed7c1e11
fabio <fabio.freitas@les.inf.puc-rio.br>
2023-07-16 19:20:10 -0300

    feat: Adicionar endpoint de estatisticas

--- core/api/viewset.py
+++ core/api/viewset.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 
 from core.models import Occurrence
+from core.service.statistic import Dashboard
 
 from .serializers import OccurrenceSerializer
 
@@ -15,3 +20,32 @@ class OcorrenceViewset(ModelViewSet):
     queryset = Occurrence.objects.all()
     serializer_class = OccurrenceSerializer
     pagination_class = StandardResultsSetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('profile__gender__name', 'risk', 'unit_type', 'type_of_occurrence', 'type_of_traffic_accident', 'date')
+
+
+    @action(detail=False, methods=['get'])
+    def get_pie_charts(self, request, pk=None):
+        '''
+        Retorna os tipos de ocorrencia.
+        '''
+    
+        qs = self.filter_queryset(self.get_queryset())
+        Dashboard.setFilteredOccurrence(qs)
+        result = Dashboard.get_statistc_piechart()
+        
+        return Response(result, status=status.HTTP_200_OK)
+
+
+    @action(detail=False, methods=['get'])
+    def get_histogram_charts(self, request, pk=None):
+        '''
+        Retorna os tipos de ocorrencia.
+        '''
+    
+        qs = self.filter_queryset(self.get_queryset())
+        Dashboard.setFilteredOccurrence(qs)
+        result = Dashboard.get_age()
+        
+        return Response(result, status=status.HTTP_200_OK)
+      
---
+++ core/service/statistic.py
@@ -0,0 +1,76 @@
+from django.db.models import Count, F
+
+from core.models import Occurrence
+
+class Dashboard:
+    
+    qs = None
+
+    @staticmethod
+    def setFilteredOccurrence(queryset):
+        Dashboard.qs = queryset
+
+    @classmethod
+    def get_statistc_piechart(cls):
+        """
+        Consolida as estatísticas de tipo de ocorrencia, tipo de acidente de transito,
+          tipo de unidade de atendimento, risco.
+          return dict  
+        """    
+        return dict(
+            type_of_occurrence = cls.get_type_of_occurrence(),
+            type_of_traffic_accident = cls.get_type_of_traffic_accident(),
+            risk = cls.get_risk(),
+            unit_type = cls.get_unit_type(),
+            gender = cls.get_gender()
+        )
+    
+    @staticmethod
+    def get_type_of_occurrence():
+        result = Dashboard.qs.exclude(type_of_occurrence__isnull=True) \
+            .values('type_of_occurrence') \
+            .annotate(name=F('type_of_occurrence__name'),value=Count('id'))
+
+        return list(result)
+    
+    @staticmethod
+    def get_type_of_traffic_accident():
+        result = Dashboard.qs.exclude(type_of_traffic_accident__isnull=True) \
+            .values('type_of_traffic_accident') \
+            .annotate(name=F('type_of_traffic_accident__name'),value=Count('id'))
+
+        return list(result)
+    
+    @staticmethod
+    def get_risk():
+        result = Dashboard.qs.exclude(risk__isnull=True) \
+            .values('risk') \
+            .annotate(name=F('risk__rating'),value=Count('id'))
+
+        return list(result)
+        
+    @staticmethod
+    def get_unit_type():
+        result =Dashboard.qs.exclude(unit_type__isnull=True) \
+            .values('unit_type') \
+            .annotate(name=F('unit_type__name'),value=Count('id'))
+
+        return list(result)
+    
+    @staticmethod
+    def get_gender():
+        result =Dashboard.qs.exclude(profile__gender__isnull=True) \
+            .values('profile__gender') \
+            .annotate(name=F('profile__gender__name'),value=Count('id'))
+
+        return list(result)
+        
+    @staticmethod
+    def get_age():
+        result =Dashboard.qs.exclude(profile__age__isnull=True) \
+            .exclude(profile__gender__isnull=True) \
+            .values('profile__age', 'profile__gender__name') \
+            .annotate(value=Count('id')).order_by("profile__age")
+
+        return list(result)
+
